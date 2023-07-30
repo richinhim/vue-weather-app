60cb4ee802ed17e81f08dab4f319e83afe1ee4f3
sooyon lee <kwlsy666@naver.com>
2023-07-30 15:38:50 +0900

    4-5.마무리 코딩 및 배포

--- src/mixins/weatherMixin.js
+++ src/mixins/weatherMixin.js
@@ -6,7 +6,7 @@ const weatherMixin = {
   },
   methods: {
     async getWeatherInfo(city) {
-      const API_KEY = "eb8c17c7e444065139587353869e392a";
+      const API_KEY = process.env.VUE_APP_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`;
       const res = await axios.get(url); // axios 사용시 promise 사용해야
       console.log(res);
